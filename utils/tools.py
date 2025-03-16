5d76ca6f0d3d25e45e87b62e9ebe5885a9ce1c17
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-03-16 15:10:17 +0700

    Update

--- utils/tools.py
+++ utils/tools.py
@@ -36,7 +36,10 @@ class EarlyStopping:
         self.counter = 0
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.Inf
+        # self.val_loss_min = np.Inf
+        # ---- NR Patch
+        self.val_loss_min = np.inf        
+        # ---- End of NR Patch
         self.delta = delta
 
     def __call__(self, val_loss, model, path):
